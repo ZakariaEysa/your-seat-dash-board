c82575c5c43c65952c88318d3281eb8bd6d44f3a
Roaa <araneem358@gmail.com>
2025-03-15 09:37:46 +0200

    movies & transaction & halls

---
+++ lib/features/admin_flow/movie_detail/view/movie_details.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:yourseatgraduationproject/features/admin_flow/movie_detail/widgets/movie_info.dart';
+import 'package:yourseatgraduationproject/features/admin_flow/movie_detail/widgets/story_line.dart';
+import '../../../../widgets/button/button_builder.dart';
+import '../widgets/story_line.dart';
+
+class MovieDetail extends StatelessWidget {
+  const MovieDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: MovieInfoScreen(),
+          ),
+          SizedBox(height: 10.h),
+
+          SizedBox(height: 4.h),
+          Expanded(
+              flex: 1,
+              child: StoryLine()
+            //StoryLine(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonBuilder(
+                  text: 'Add Movie',
+                  onTap: () {},
+                  width: 40.w,
+                  height: 50.h,
+                  buttonColor: Color(0xFF560B76),
+                  borderShape: BorderRadius.circular(10.r),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 7.sp,
+                  ),
+                ),
+                SizedBox(width: 10.w),
+                ButtonBuilder(
+                  text: 'cancel',
+                  onTap: () {},
+                  width: 40.w,
+                  height: 50.h,
+                  buttonColor: Color(0xFF560B76),
+                  borderShape: BorderRadius.circular(10.r),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 7.sp,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/text_field/text_field/new_text_field_builder.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class NewTextField extends StatelessWidget {
+  final bool? obscureText; // جعله اختياريًا
+  final String? hintText; // جعله اختياريًا
+  final Color borderColor; // لون البورد يكون مطلوبًا دائمًا
+
+  const NewTextField({
+    super.key,
+    this.obscureText,
+    this.hintText,
+    this.borderColor=Colors.black, // borderColor مطلوب دائمًا
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscureText ?? false, // إذا كان null، استخدم false كقيمة افتراضية
+      style: TextStyle(fontSize: 5.sp, color: Colors.black),
+      textAlign: TextAlign.start,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        hintText: hintText, // hintText اختياري
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.r),
+          borderSide: BorderSide(
+            color: borderColor, // لون البورد من المتغير
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0.r),
+          borderSide: BorderSide(
+            width: 1,
+            color: borderColor, // لون البورد من المتغير
+          ),
+        ),
+      ),
+    );
+  }
+}
