8f2757403f7bb6951416087f8e7d19af6ae4aacb
Roaa <araneem358@gmail.com>
2025-05-04 22:31:04 +0300

    edit in halls

--- lib/main.dart
+++ lib/main.dart
@@ -91,7 +91,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: "Admin Dashboard",
             theme: ThemeData.dark(),
-            home: SignIn(),
+            home: NavigationList(),
           ),
         );
       },
